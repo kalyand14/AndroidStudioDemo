2ecb6451b4c742a3a18f15b3785c2c7c577d4f1c
kalyan <kalyand14@gmail.com>
2015-01-03 19:40:43 +0530

    test  commit

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+
+        ////Test Commit
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
